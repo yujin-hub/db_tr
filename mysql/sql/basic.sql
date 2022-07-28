17d1b1f79e0e9b514f698d8ae849dfc05b4268a6
cuj0405@naver.com <cuj0405@naver.com>
2022-07-28 14:15:59 +0900

    .

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,3 +1,20 @@
 -- olive database를 사용
-use olive;
+USE olive;
 
+-- 전체 컬럼 조회
+SELECT * FROM user;
+
+-- 컬럼 추가
+ALTER TABLE member2 ADD column nickname VARCHAR(45);
+ALTER TABLE member2 ADD column name VARCHAR(45);
+ALTER TABLE member2 ADD column ID VARCHAR(45);
+ALTER TABLE member2 ADD column nameEng VARCHAR(45) AFTER name;
+ALTER TABLE member2 ADD column nameChn VARCHAR(45) AFTER name;
+ALTER TABLE member2 ADD column dob DATE;
+ALTER TABLE member2 ADD column gender TINYINT;
+
+-- 컬럼 변경 (자료형을 바꾸는 경우 데이터가 작성되어 있으면 잘 안될수도)
+ALTER TABLE member2 MODIFY column nickname varchar(100);
+
+-- 컬럼 이름 변경
+ALTER TABLE member2 CHANGE column nickname nick VARCHAR(45);
