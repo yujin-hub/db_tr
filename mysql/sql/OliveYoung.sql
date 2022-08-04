7078c2400851f218d070fd8b963f8474de67fc0b
cuj0405@naver.com <cuj0405@naver.com>
2022-08-04 15:48:35 +0900

    .

--- mysql/sql/OliveYoung.sql
+++ mysql/sql/OliveYoung.sql
@@ -1,14 +1,30 @@
 use olive;
 
 
+SELECT
+	a.seq
+    , a.property
+    , b.seq
+    , b.name
+FROM CodeGroup a
+-- LEFT JOIN Code b on a.seq = b.CodeGroup_seq
+INNER JOIN Code b on a.seq = b.CodeGroup_seq
+-- JOIN Code b on a.seq = b.CodeGroup_seq
+;
+
+
 -- 로그인
 SELECT * FROM user WHERE id = "cuj" AND pw="hello"
 ; 
 
 -- 회원가입
 
+
 -- 상품 목록
+
 
 -- 상품 상세
 
+
 -- 구매
+
