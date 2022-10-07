3891ff362add057f5a7dd0fdcc6b23991d6b55f6
cuj0405@naver.com <cuj0405@naver.com>
2022-10-07 16:58:43 +0900

    .

--- mysql/sql/OliveYoung.sql
+++ mysql/sql/OliveYoung.sql
@@ -166,7 +166,7 @@ WHERE 1=1
 
 -- 회원가입
 INSERT INTO user (
-		name
+		name 
         , gender
         , dob
         , tel
@@ -935,5 +935,51 @@ select * from CodeGroup;
 select * from user;
 select * from shipping_addr;
 select * from brand_list;
+select * from itemUploaded;
+select * from userUploaded;
+select * from productUploaded;
+
+
+
+
+CREATE TABLE `userUploaded` (
+  `seq` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `type` tinyint(4) DEFAULT NULL,
+  `defaultNy` tinyint(4) DEFAULT NULL,
+  `sort` tinyint(4) DEFAULT NULL,
+  `path` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `originalName` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `uuidName` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ext` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `size` bigint(20) DEFAULT NULL,
+  `delNy` tinyint(4) NOT NULL,
+  `pseq` bigint(20) NOT NULL,
+  `regIp` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `regSeq` bigint(20) DEFAULT NULL,
+  `regDeviceCd` int(11) DEFAULT NULL,
+  `regDateTime` datetime DEFAULT NULL,
+  `regDateTimeSvr` datetime DEFAULT NULL,
+  PRIMARY KEY (`seq`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `productUploaded` (
+  `seq` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `type` tinyint(4) DEFAULT NULL,
+  `defaultNy` tinyint(4) DEFAULT NULL,
+  `sort` tinyint(4) DEFAULT NULL,
+  `path` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `originalName` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `uuidName` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ext` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `size` bigint(20) DEFAULT NULL,
+  `delNy` tinyint(4) NOT NULL,
+  `pseq` bigint(20) NOT NULL,
+  `regIp` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `regSeq` bigint(20) DEFAULT NULL,
+  `regDeviceCd` int(11) DEFAULT NULL,
+  `regDateTime` datetime DEFAULT NULL,
+  `regDateTimeSvr` datetime DEFAULT NULL,
+  PRIMARY KEY (`seq`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
